cd76ada2e4642868b7bbbcee6729663b349f166f
Ellen Conley <conley.eg@gmail.com>
2020-07-03 14:14:55 -0700

    troubleshooting call production api issue

--- src/views/About.vue
+++ src/views/About.vue
@@ -25,7 +25,7 @@ export default {
     callAPI() {
       axios({
         method: "GET",
-        url: `${process.env.VUE_APP_SECRET_AUDIENCE}/helloworld`,
+        url: `https://vue-spring-kotlin-graphql-back.herokuapp.com/helloworld`,
         headers: { authorization: `Bearer ${this.accessToken}` }
       })
         .then(resp => {
